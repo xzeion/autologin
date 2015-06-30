52f7b5604d4666b3ede6cfab6e2f939ff4d6a9de
Xzeion <xzeion@gmail.com>
2015-06-30 00:06:51 -0400

    added files needed to create a chrome extension

---
+++ login.js
@@ -0,0 +1,13 @@
+
+var user = "";
+var pass = "";
+
+
+function login(user, pass) {
+    if(document.URL
+    $('div.email-address input').text(user);
+    $('div.password input').text(pass);
+    $('button.submit').click()
+}
+
+setTimeout(login, 10000)
